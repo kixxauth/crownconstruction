e7d12267aa8d3a2c14a5ccc01dc5931fc1677f10
Kristoffer Walker <kixxauth@gmail.com>
2009-09-10 05:59:51 -0400

    new: kinds and predicates for Contract in model
     On branch master
    	modified:   modules/model.js

--- firefox/modules/model.js
+++ firefox/modules/model.js
@@ -6,6 +6,69 @@ const Cu = Components.utils;
 
 Cu.import("resource://crownconstruction/modules/lib.js");
 
+let kind_Contract =
+{
+  uri:'http://www.crownconstructioninc.com/rdf#Contract',
+
+  properties:
+  {
+    Salesperson:'http://www.crownconstructioninc.com/rdf#hasSalesperson',
+    Estimator:'http://www.crownconstructioninc.com/rdf#hasEstimator',
+    Production:'http://www.crownconstructioninc.com/rdf#hasProduction',
+    contractDate:'http://www.fireworksproject.com/rdf#hasDate',
+    startDate:'http://www.fireworksproject.com/rdf#hasDate',
+    completeDate:'http://www.fireworksproject.com/rdf#hasDate',
+    cost:'http://www.fireworksproject.com/rdf#hasAmount',
+    estimatedGP:'http://www.fireworksproject.com/rdf#hasAmount',
+    taxlabor:'http://www.fireworksproject.com/rdf#hasAmount',
+    Customer:'http://www.crownconstructioninc.com/rdf#hasCustomer',
+    Job:'http://www.crownconstructioninc.com/rdf#hasJob',
+    SpecialOrder:'http://www.crownconstructioninc.com/rdf#hasSpecOrder',
+    SubContractor:'http://www.crownconstructioninc.com/rdf#hasSpecOrder',
+  }
+};
+
+let kind_SubContractor =
+{
+  uri:'http://www.crownconstructioninc.com/rdf#SubContractor',
+
+  properties:
+  {
+    name:'http://www.fireworksproject.com/rdf#hasName',
+    task:'http://www.fireworksproject.com/rdf#hasDescription',
+    phonenumber:'http://www.fireworksproject.com/rdf#hasPhoneNumber',
+    quote:'http://www.fireworksproject.com/rdf#hasAmount',
+    startDate:'http://www.fireworksproject.com/rdf#hasDate'
+  }
+};
+
+let kind_SpecialOrder =
+{
+  uri:'http://www.crownconstructioninc.com/rdf#SpecialOrder',
+
+  properties:
+  {
+    name:'http://www.fireworksproject.com/rdf#hasName',
+    vendor:'http://www.fireworksproject.com/rdf#hasName',
+    orderedBy:'http://www.fireworksproject.com/rdf#hasName',
+    orderDate:'http://www.fireworksproject.com/rdf#hasDate',
+    deliveryDate:'http://www.fireworksproject.com/rdf#hasDate'
+  }
+};
+
+let kind_Job =
+{
+  uri:'http://www.crownconstructioninc.com/rdf#Job',
+
+  properties:
+  {
+    idno:'http://www.fireworksproject.com/rdf#hasName',
+    type:'http://www.fireworksproject.com/rdf#hasStringTag',
+    retail:'http://www.fireworksproject.com/rdf#hasAmount',
+    foreman:'http://www.crownconstructioninc.com/rdf#hasForeman'
+  }
+};
+
 let kind_Employee =
 {
   uri:'http://www.crownconstructioninc.com/rdf#Employee',
@@ -81,6 +144,66 @@ let kind_Email =
 
 let predicates =
 {
+    'http://www.fireworksproject.com/rdf#hasName':
+    {
+      range:'literal',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasSpecOrder':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#SpecialOrder',
+      cardinal:"*"
+    },
+    'http://www.crownconstructioninc.com/rdf#hasSubContractor':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#SubContractor',
+      cardinal:"*"
+    },
+    'http://www.fireworksproject.com/rdf#hasDescription':
+    {
+      range:'literal',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasForeman':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Employee',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasJob':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Job',
+      cardinal:"*"
+    },
+    'http://www.crownconstructioninc.com/rdf#hasCustomer':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Customer',
+      cardinal:1
+    },
+    'http://www.fireworksproject.com/rdf#hasAmount':
+    {
+      range:'literal',
+      cardinal:1
+    },
+    'http://www.fireworksproject.com/rdf#hasDate':
+    {
+      range:'literal',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasEstimator':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Employee',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasProduction':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Employee',
+      cardinal:1
+    },
+    'http://www.crownconstructioninc.com/rdf#hasSalesperson':
+    {
+      range:'http://www.crownconstructioninc.com/rdf#Employee',
+      cardinal:1
+    },
     'http://www.fireworksproject.com/rdf#hasStringTag':
     {
       range:'literal',
@@ -163,6 +286,10 @@ let model = {};
 
 model.kinds =
 {
+  Job: kind_Job,
+  Contract: kind_Contract,
+  SubContractor: kind_SubContractor,
+  SpecialOrder: kind_SpecialOrder,
   Employee: kind_Employee,
   Customer: kind_Customer,
   Person: kind_Person,
