e4067cea887cca0d0ff4e1dc9a38d973623850a1
Kristoffer Walker <kixxauth@gmail.com>
2010-02-24 12:33:35 -0500

    Most recent prototype content.
     On branch rc1
    	modified:   firefox/content/prototype.js

--- firefox/content/prototype.js
+++ firefox/content/prototype.js
@@ -1,2 +1,26 @@
 var Q = require("events");
 var HTTP = require("http");
+
+var P;
+
+window.addEventListener("load",
+    function (e) {
+      var r = HTTP.create();
+      var p = r.send("GET", "http://www.foodoes_not_exist/");
+      dump("call to r.send() returned\n");
+      p.then(
+        function (res) {
+          dump("RESPONSE status:\n"+ res.status +"\n");
+          res.headers = 44;
+        },
+        function (e) {dump("Got exception. \n"+ e +"\n");}).
+      then(
+        function (res) {
+          dump("RESPONSE HEADERS:\n"+ res.headers +"\n");
+          res.headers = 7;
+        },
+        function (e) {dump("Got exception. \n"+ e +"\n");}
+        );
+      dump("call to r.then() returned\n");
+      P = p;
+    }, false);
