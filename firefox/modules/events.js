9e11343686d1df4985d641cab31fadab5f904d9c
Kristoffer Walker <kixxauth@gmail.com>
2010-02-24 12:27:56 -0500

    Events module seems ready for action.
     On branch rc1
    	modified:   firefox/modules/events.js

--- firefox/modules/events.js
+++ firefox/modules/events.js
@@ -8,329 +8,228 @@ bitwise: true,
 strict: true,
 immed: true */
 
-/*members apply, baseEvent, broadcast, call, cancelDefault, constructor, 
-    dismiss, emit, emitter, event, getDefaulting, getPropagation, import, 
-    indexOf, length, observe, promise, prototype, push, require, setSignal, 
-    signal, slice, splice, state, stopPropagation, then, toString, tower, 
-    tuneIn, tuneOut, utils, values
+/*members "@mozilla.org\/thread-manager;1", DISPATCH_NORMAL, apply, 
+    baseEvent, call, classes, constructor, dispatch, eventConstructor, 
+    getDefaulting, getPropagation, getService, id, import, interfaces, 
+    length, listen, mainThread, notify, nsIThread, nsIThreadManager, ok, 
+    promise, prototype, push, require, run, send, slice, then, toString, 
+    utils, watch
 */
 
 /*global
-Components: false
+Components: false,
+setTimeout: false
 */
 
 "use strict";
 
+// For Mozilla JavaScript modules system.
 var EXPORTED_SYMBOLS = ["exports"];
 
-if (typeof Components !== "undefined") {
+// If we are in the Mozilla module system we need to add some boilerplate to be
+// CommonJS complient. This is obviously an ugly hack to allow integration with
+// legacy code that uses the Mozilla module system.
+if (Components) {
   var require = Components.utils.import(
         "resource://crownconstruction/modules/boot.js", null).require;
   var exports = {};
+  var module = {id: "events"};
 }
+
+var ASSERT = require("assert");
+
+var queue = (function create_queue() {
+  var thread_manager = Components.classes["@mozilla.org/thread-manager;1"].
+                     getService(Components.interfaces.nsIThreadManager),
+      mode = Components.interfaces.nsIThread.DISPATCH_NORMAL;
+
+  return function global_queue(continuation, event, args) {
+          thread_manager.mainThread.dispatch({
+                         run: function enqueue() {
+                             continuation.apply(event, args);
+                           }
+                         }, mode);
+        };
+}());
 
 /**
  * The base event prototype.
  */
 exports.baseEvent = function event_constructor() {
-  var self = {},
-      propagation = true,
-      defaulting = true;
-
-  self.stopPropagation = function stop_propagation() {
-    propagation = false;
-  };
-
-  self.cancelDefault = function cancel_default() {
-    defaulting = false;
-  };
-
-  self.getPropagation = function get_propagation() {
-    return propagation;
-  };
-
-  self.getDefaulting = function get_defaulting() {
-    return defaulting;
-  };
+  var self = {};
 
   self.toString = function to_string() {
-    return '{"constructor": "baseEvent", "propagation": '+
-           propagation +', "defaulting": '+ defaulting +'}';
+    return '{"constructor": "baseEvent"}';
   };
 
   self.constructor = exports.baseEvent;
   return self;
 };
 
-/**
- * A signal can be sent from point to point or it can be relayed out to a
- * network of towers that are tuned into it.
- */
-exports.signal = function signal_constructor(action, eventConstructor) {
+function signal_constructor(spec) {
+  spec = spec || {};
   var self = {},
-      q = [];
+      q = [],
+      event_constructor = spec.eventConstructor || exports.baseEvent;
 
-  eventConstructor = eventConstructor || exports.baseEvent;
-
-  /**
-   * A tower calls this method to listen for this signal and possibly relay it
-   * to more towers.
-   */
-  self.tuneIn = function tune_in(observer) {
-    var s = exports.signal(observer);
-    q.push(s.emitter());
-    return s;
+  self.listen = function signal_listen(observer) {
+    q.push(observer);
   };
 
-  /**
-   * A tower calls this methed to ignore this signal if it has previously been
-   * tuned into it.
-   */
-  self.tuneOut = function tune_out(observer) {
-    var i = q.indexOf(observer);
-    if (i >= 0) {
-      q.splice(i, 1);
-    }
-  };
-
-  /**
-   * A tower calls this method to broadcast this signal.
-   */
-  self.emit = function emit() {
-    var i, len = q.length, event;
-
-    if (this.constructor === eventConstructor) {
-      event = this;
-    } else {
-      event = eventConstructor();
-    }
+  self.send = function signal_send() {
+    var len = q.length, i,
+        event = ((this.constructor === event_constructor) && this ||
+                 (typeof this.getPropagation === "function" &&
+                  typeof this.getDefaulting === "function") && this ||
+                 event_constructor(this));
 
     for (i = 0; i < len; i += 1) {
-      if (!event.getPropagation()) {
-        break;
-      }
-      q[i].apply(event, arguments);
-    }
-
-    if (typeof action === "function" && event.getDefaulting()) {
-      return action.apply(event, arguments);
+      q[i].apply(event, Array.prototype.slice.call(arguments));
     }
   };
 
-  /**
-   * Create a function that emits this signal when it is called.
-   */
-  self.emitter = function create_emitter() {
-    return function emitter() {
-      return self.emit.apply(this, arguments);
-    };
-  };
-
-  self.constructor = exports.signal;
+  self.constructor = signal_constructor;
   return self;
-};
+}
 
-/**
- * Construct a tower instance that observes and broadcasts signals.
- */
-exports.tower = function tower_constructor(signal_names) {
-  var self = {},
-      signals = {}, i;
+function beacon_constructor(spec, id) {
+  var self = {}, signals = {}, len = spec.length, i;
+  id = id || "unidentified";
 
-  for (i = 0; i < signal_names.length; i += 1) {
-    signals[signal_names[i]] = exports.signal();
+  for (i = 0; i < len; i += 1) {
+    signals[spec[i]] = signal_constructor();
   }
 
-  /**
-   * Create a named signal for this tower to broadcast or observe.
-   *
-   * @param {string} name The name string that identified the signal.
-   *
-   * @param {object|function} signal If signal is an instance of signal() it
-   * will be used as signal on this tower for the given name. If not, it must
-   * be a function that will become the default action on a newly created
-   * signal using the given eventConstructor if it is provided. The default
-   * action fuction of a signal is called with the event object bound to
-   * <code>this</code>.
-   *
-   * @param {function} [eventConstructor] Only required if the signal argument
-   * is not an instance of signal(). It is the constructor used for the event
-   * that will ride on the newly created signal.
-   */
-  self.setSignal = function set_signal(name, signal, eventConstructor) {
-    if (signal.constructor !== exports.signal) {
-      signal = exports.signal(signal, eventConstructor);
+  self.notify = function beacon_watch(name) {
+    if (!signals[name]) {
+      throw new Error("There is no signal known by '"+ name +
+                      "' on beacon '"+ id +"'.");
     }
-    signals[name] = signal;
-    return signal;
+    signals[name].send.apply(
+        exports.baseEvent(), Array.prototype.slice.call(arguments, 1));
   };
 
-  /**
-   * Set an observer on this tower tuned into the named signal.
-   *
-   * Observers set using this method will be called before the default action
-   * of the signal.
-   *
-   * @param {string} name The name of the signal to observe on this tower.
-   * @param {function} observer The function that will become the default
-   * action of a newly created signal.
-   *
-   * @returns {signal} The newly created signal.
-   */
-  self.observe = function observe(name, observer) {
-    return signals[name].tuneIn(observer);
-  };
-
-  /**
-   * Remove an observer from this tower that is tuned into the named signal.
-   *
-   * If the observer does not exist on the named signal, this method has no
-   * effect.  However, if the named signal does not exist, it will raise a
-   * TypeError.
-   *
-   * @param {string} name The name of the signal.
-   * @param {function} observer The function to remove.
-   */
-  self.dismiss = function dismiss(name, observer) {
-    signals[name].tuneOut(observer);
-  };
-
-  /**
-   * Broadcast a signal by the given name.
-   *
-   * @param {string} name The name of the signal to broadcast on this tower.
-   */
-  self.broadcast = function broadcast(name) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return signals[name].emit.apply(signals[name], args);
-  };
-
-  self.constructor = exports.tower;
-  return self;
-};
-
-function initialized_promise_constructor(spec) {
-  var self = {},
-      tower = spec.tower,
-      event = spec.event,
-      values = spec.values,
-      inprogress = 2,
-      fulfilled = 3,
-      exception = 4,
-      state = spec.state;
-
-  self.then = function then(fulfill, except, progress) {
-    var new_promise;
-
-    if (typeof fulfill === "function") {
-      if (state === fulfilled) {
-        fulfill.apply(event, values);
-      } else {
-        tower.observe("fulfill", fulfill);
-      }
+  self.watch = function beacon_watch(name, observer) {
+    if (!signals[name]) {
+      throw new Error("There is no signal known by '"+ name +
+                      "' on beacon '"+ id +"'.");
     }
-    if (typeof except === "function") {
-      if (state === exception) {
-        except.apply(event, values);
-      } else {
-        tower.observe("exception", except);
-      }
+    if (typeof observer !== "function") {
+      throw new Error("The observer passed to beacon.listen() for signal '"+
+                      name +"' must be a function.");
     }
-    if (typeof progress === "function") {
-      if (state === inprogress) {
-        progress.apply(event, values);
-      }
-      tower.observe("progress", progress);
-    }
-    new_promise = initialized_promise_constructor({tower: tower,
-                                                   state: state,
-                                                   event: event,
-                                                   values: values});
-    tower.broadcast("register");
-    return new_promise;
+    signals[name].listen(observer);
   };
 
-  self.constructor = exports.promise;
+  self.constructor = beacon_constructor;
   return self;
 }
 
 exports.promise = function promise_constructor(init, id) {
+  if (typeof init !== "function") {
+    throw new Error(
+        "init routine passed to promise() constructor must be a function.");
+  }
+
+  id = id || "unidentified";
+
   var self = {},
-      tower = exports.tower(["progress", "exception", "fulfill", "register"]),
-      event, values,
       initialized = 1,
       inprogress = 2,
       fulfilled = 3,
       exception = 4,
+      beacon = beacon_constructor(["fulfill", "exception", "progress"], id),
+      event,
+      values,
       state = 0;
 
-  id = ((typeof id === "undefined") ? "unidentified" : id);
+  beacon.watch("fulfill", function promise_fulfill() {
+        ASSERT.ok(state >= initialized, "The promise '"+ id +"' was notified "+
+          "of fulfillment before reaching the initialized state.");
+        ASSERT.ok(state <= fulfilled, "The promise '"+ id +"' was notified "+
+          "of fulfillment after already reaching the fulfilled state.");
 
-  tower.observe("progress", function progress_default() {
-        if (state >= fulfilled) {
-          throw new Error("Promise '"+ id +
-            "' has already reached a fulfilled or exception state.");
-        }
-        event = this;
-        values = Array.prototype.slice.call(arguments, 0);
-        state = inprogress;
-      });
-
-  tower.observe("fulfill", function exception_default() {
-        if (state >= fulfilled) {
-          throw new Error("Promise '"+ id +
-            "' has already reached a fulfilled or exception state.");
-        }
-        event = this;
-        values = Array.prototype.slice.call(arguments, 0);
-        state = exception;
-      });
-
-  tower.observe("fulfill", function fulfill_default() {
-        if (state >= fulfilled) {
-          throw new Error("Promise '"+ id +
-            "' has already reached a fulfilled or exception state.");
-        }
-        event = this;
-        values = Array.prototype.slice.call(arguments, 0);
         state = fulfilled;
+
+        // The beacon will call apply() on this function, binding the
+        // event object to "this".
+        event = this;
+        
+        values = Array.prototype.slice.call(arguments);
       });
 
-  init(tower);
+  beacon.watch("exception", function promise_exception() {
+        ASSERT.ok(state >= initialized, "The promise '"+ id +"' was notified "+
+          "of an exception before reaching the initialized state.");
+        ASSERT.ok(state <= fulfilled, "The promise '"+ id +"' was notified "+
+          "of an exception after already reaching the fulfilled state.");
 
-  self.then = function then(fulfill, except, progress) {
-    var new_promise;
+        state = exception;
 
+        // The beacon will call apply() on this function, binding the
+        // event object to "this".
+        event = this;
+        
+        values = Array.prototype.slice.call(arguments);
+      });
+
+  beacon.watch("progress", function promise_progress() {
+        ASSERT.ok(state >= initialized, "The promise '"+ id +"' was notified "+
+          "of progress before reaching the initialized state.");
+        ASSERT.ok(state <= fulfilled, "The promise '"+ id +"' was notified "+
+          "of an progress after already reaching the fulfilled state.");
+
+        state = inprogress;
+
+        // The beacon will call apply() on this function, binding the
+        // event object to "this".
+        event = this;
+        
+        values = Array.prototype.slice.call(arguments);
+      });
+
+  /**
+   * Each callback continuation function passed to promise.then() will be
+   * called with the arguments applied by the event initializer.  Also,
+   * promise.then() returns a reference to the promise instance so that
+   * promise.then() can be called in a chain like promise.then().then().then();
+   * or imperitively like promise.then(); promise.then(); promise.then();. One
+   * additional feature is that the internally created event object is bound to
+   * "this" within each of the callback functions passed to promise.then(). The
+   * last characteristic to keep in mind is that reference values like objects
+   * and arrays passed to the callbacks registered with promise.then() my have
+   * been mutated by a previous callback in the chain and cannot necessarily be
+   * trusted.
+   */
+  self.then = function promise_then(fulfill, except, progress) {
     if (typeof fulfill === "function") {
       if (state === fulfilled) {
         fulfill.apply(event, values);
       } else {
-        tower.observe("fulfill", fulfill);
+        beacon.watch("fulfill", fulfill);
       }
     }
     if (typeof except === "function") {
       if (state === exception) {
         except.apply(event, values);
       } else {
-        tower.observe("exception", except);
+        beacon.watch("exception", except);
       }
     }
     if (typeof progress === "function") {
       if (state === inprogress) {
         progress.apply(event, values);
       }
-      tower.observe("progress", progress);
+      beacon.watch("progress", progress);
     }
-    new_promise = initialized_promise_constructor({tower: tower,
-                                                   state: state,
-                                                   event: event,
-                                                   values: values});
-    tower.broadcast("register");
-    return new_promise;
+    return self;
   };
 
-  state = initialized;
   self.constructor = exports.promise;
+  queue(function promise_init() {
+          state = initialized;
+          init(beacon);
+        });
   return self;
 };
 
