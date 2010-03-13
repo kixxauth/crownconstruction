23595e77dde35d61d95a8a8be97639e98cb79f4a
Kristoffer Walker <kixxauth@gmail.com>
2010-03-13 15:27:55 -0500

    App init routine.
     On branch rc1
    	modified:   firefox/content/app.html
    	modified:   firefox/content/app.js
    	new file:   firefox/content/css/app.css
    	modified:   firefox/modules/worker.js

--- firefox/modules/worker.js
+++ firefox/modules/worker.js
@@ -54,14 +54,179 @@ if (Components) {
   var module = {id: "worker"};
 }
 
+var CONF = require("configs");
 var ASSERT = require("assert");
+var LOGGING = require("logging");
+var EVENTS = require("events");
 var DDD = require("dcube");
 
-var LOGIN;
+DDD.setDomain(CONF.get("data-url"));
 
-exports.init = function pub_init(cc) {
-  if (!LOGIN) {
-    cc("login");
+var LOGGER = LOGGING.getLogger("CrownERM.worker"),
+    DBNAME = CONF.get("dbname"),
+    CXN;
+
+/**
+ */
+exports.init = (function () {
+  var login, init, load,
+      promise, beacon, state = 0,
+      initialize = 1,
+      authenticating = 10,
+      authenticated = 20,
+      loading = 30,
+      loaded = 40,
+      exception = 90,
+      done = 100,
+
+      events = {
+        "1": "login",
+        "9": "login-error",
+        "10": "loading",
+        "11": "bad-credentials",
+        "20": "loading",
+        "30": "loading",
+        "40": "loading"
+      };
+
+  login = function login(username, passkey, dbname) {
+    try {
+      username = DDD.validateUsername(username);
+    } catch (usernameEx) {
+      switch (usernameEx.message) {
+      case "too short":
+        beacon.progress(events[9], "username too short", login);
+        break;
+      case "too long":
+        beacon.progress(events[9], "username too long", login);
+        break;
+      case "invalid characters":
+        beacon.progress(events[9], "username has bad characters", login);
+        break;
+      default:
+        LOGGER.warn("Unexpected DCube username validation error '"+
+          usernameEx.message +"'");
+        state = exception;
+        beacon.exception(new Error("unknown username exception"));
+      }
+      return;
+    }
+
+    try {
+      passkey = DDD.validatePasskey(passkey);
+    } catch (passkeyEx) {
+      switch (passkeyEx.message) {
+      case "too short":
+        beacon.progress(events[9], "passkey too short", login);
+        break;
+      case "too long":
+        beacon.progress(events[9], "passkey too long", login);
+        break;
+      default:
+        LOGGER.warn("Unexpected DCube passkey validation error '"+
+          passkeyEx.message +"'");
+        state = exception;
+        beacon.exception(new Error("unknown passkey exception"));
+      }
+      return;
+    }
+
+    state = authenticating;
+    dbname = dbname || DBNAME;
+    load(dbname, username, passkey);
+  };
+
+  load = function load(dbname, username, passkey) {
+    var query = DDD.query("query").appendStatement("class", "=", "job");
+    DDD.connect(dbname, username, [query], passkey).then(
+
+        function connect_fulfilled(rv) {
+          state = authenticated;
+          CXN = rv.connection;
+          state = loading;
+          beacon.progress(events[state]);
+          // TODO: Do something with rv.results here; set a global cache.
+          state = loaded;
+          beacon.fulfill();
+          state = done;
+        },
+
+        function connect_exception(ex) {
+          switch (ex.name +":"+ ex.message) {
+          case "DCubeException:user does not exist":
+            beacon.progress(events[11], "user does not exist", login);
+            break;
+          case "DCubeException:invalid passkey":
+            beacon.progress(events[11], "invalid passkey", login);
+            break;
+          case "DCubeException:not found":
+            LOGGER.warn("Invalid database.");
+            state = exception;
+            beacon.exception(new Error("invalid database"));
+            break;
+          default:
+            LOGGER.warn("Unexpected DCube connection error "+ ex);
+            state = exception;
+            beacon.exception(new Error("unknown DCube exception"));
+          }
+        });
+
+  };
+
+  init = function init(b) {
+    try {
+      ASSERT.equal(beacon, undefined,
+          "worker.init() beacon must not be assigned.");
+      ASSERT.equal(CXN, undefined,
+          "worker.init() LOGIN must not be assigned.");
+      ASSERT.equal(state, 0,
+          "worker.init() state must be 0.");
+    } catch (assertion) {
+      LOGGER.error(assertion);
+      state = exception;
+      b.exception(assertion);
+      return;
+    }
+
+    state = initialize;
+    beacon = b;
+    beacon.progress(events[state], login);
+  };
+
+  return function pub_init() {
+      if(state === exception) {
+        promise = undefined;
+        beacon = undefined;
+        CXN = undefined;
+        state = 0;
+      }
+      if(!promise) {
+        promise = EVENTS.promise(init);
+      }
+      return promise;
+    };
+}());
+
+/**
+ */
+exports.debug = function pub_debug(debug) {
+  if (debug) {
+    LOGGING.debug(1);
+    DDD.debug(1);
+  }
+  else {
+    LOGGING.debug(0);
+    DDD.debug(0);
   }
 };
 
+/**
+ */
+exports.databaseSandbox = function pub_database(toggle) {
+  if (toggle) {
+    DBNAME = CONF.get("sandbox-dbname");
+  }
+  else {
+    DBNAME = CONF.get("dbname");
+  }
+};
