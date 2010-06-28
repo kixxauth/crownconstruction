ba7abbf92763a7a42d37a4e9afcc9cfe62524c79
Kristoffer Walker <kixxauth@gmail.com>
2010-06-28 05:47:27 -0400

    Start of login -- bug in events::Promise
     On branch v0.1
    	modified:   content/app.js
    	modified:   content/crown_construction.xhtml
    	modified:   lib/db.js
    	modified:   lib/dcube.js
    	modified:   lib/events.js

--- firefox-extension/content/app.js
+++ firefox-extension/content/app.js
@@ -13,7 +13,9 @@ laxbreak: true
 
 /*global
 Components: false,
-jQuery: false
+jQuery: false,
+SHARED: false,
+MODELS: false
 */
 
 "use strict";
@@ -49,17 +51,16 @@ INIT = function (jq) {
     log.info('Module system bootstrapped.');
     platform.pref('dev', function (dev_pref) {
       if (dev_pref.value()) {
-        jq('#overlay')
+        jq('#underlay')
           .load(DEV_OVERLAY, function () {
               jq(this)
                 .children('.gridcol')
                 .css({
-                    'background': '#057dd4'
+                    'background': '#d0e2f3'
                   , 'float': 'left'
                   , 'width': '7.25%'
                   , 'height': '780px'
                   , 'margin-left': '1%'
-                  , 'opacity': '0.2'
                   })
                 ;
             })
@@ -67,9 +68,9 @@ INIT = function (jq) {
               'position': 'absolute'
             , 'top': '0'
             , 'left': '0'
-            , 'display': 'block'
             , 'width': '100%'
             , 'height': '780px'
+            , 'z-index': '-1'
             })
           ;
       }
@@ -88,18 +89,84 @@ LOGIN = function (require, log, jq, deck) {
     ;
 
   function show_login() {
+    var jq_login_warn = jq('#login-warning-box').hide()
+      ;
+
+    function show_username_warning(msg) {
+      jq_login_warn.html(msg).show();
+    }
+
+    function show_passkey_warning(msg) {
+      jq_login_warn.html(msg).show();
+    }
+
+    function try_login(username, passkey) {
+      // TODO: Cache key of user session data.
+      var query = db.Query()
+        , promise
+        ;
+
+      query.query()
+        .eq('kind', 'user_session')
+        .eq('user', username)
+        ;
+
+      // TODO: Get dbname from login form.
+      promise = db.connect('crown_construction_sandbox'
+                         , MODELS
+                         , username
+                         , passkey
+                         , query);
+      promise(
+        function (connection) {
+          logging.checkpoint('connection', connection);
+        }
+      , function (err) {
+          logging.checkpoint('connection err', err);
+        }
+      );
+    }
+
+    function handle_cmd(ev) {
+      var username = jq('#username').val()
+        , passkey = jq('#passkey').val()
+        ;
+
+      try {
+        username = SHARED.validateString(username, 1, 70, /\W/);
+      }
+      catch (u_err) {
+        // 'too short', 'too long', 'invalid characters'
+        show_username_warning(u_err);
+        return false;
+      }
+
+      try {
+        passkey = SHARED.validateString(passkey, 4, 140, /[\b\t\v\f\r\n]/);
+      }
+      catch (p_err) {
+        // 'too short', 'too long', 'invalid characters'
+        show_passkey_warning(p_err);
+        return false;
+      }
+
+      try_login(username, passkey);
+
+      return false;
+    }
+
+    jq('#login-button').click(handle_cmd);
     deck('login');
-    logging.checkpoint('overlay loaded');
   }
 
   db.connections(function (connections) {
     log.info(connections.length +' connections.');
     if (connections.length) {
-      dump('TODO: connections login.\n');
+      // TODO: connections login.
     }
     else {
       logging.checkpoint('loading overlay');
-      jq('#login').load(LOGIN_OVERLAY, show_login)
+      jq('#login').load(LOGIN_OVERLAY, show_login);
     }
   });
 };
--- firefox-extension/lib/events.js
+++ firefox-extension/lib/events.js
@@ -86,6 +86,8 @@ exports.Promise = function (init) {
         }
       };
 
+  var logging = require('logging');
+
   function broadcast(type, args) {
     if (spec.fulfilled_val || spec.exception_val) {
       return;
@@ -111,10 +113,14 @@ exports.Promise = function (init) {
       val = spec.exception_val = Array.prototype.slice.call(args);
     }
 
-    for (i = 0; i < len; i += 1) {
+    function queue_observer(fn, val) {
       exports.enqueue(function () {
-        observers[i].apply(null, val);
+        fn.apply(null, val);
       }, 0);
+    }
+
+    for (i = 0; i < len; i += 1) {
+      queue_observer(observers[i], val);
     }
   }
 
@@ -130,10 +136,12 @@ exports.Promise = function (init) {
     broadcast('progress', arguments);
   }
 
-  exports.enqueue(function init_promise() {
+  exports.enqueue(function () {
     init(broadcast_fulfill,
         broadcast_exception, broadcast_progress);
   }, 0);
+
+  return construct_pub_promise(spec);
 };
 
 function Observer(topic, callback) {
